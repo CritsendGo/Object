11103e6e70879525e3020009ef973ca674c1a300
BLETOUS <bletous@gmail.com>
2021-08-05 10:06:14 +0200

    Update postfix.go

--- postfix.go
+++ postfix.go
@@ -1,11 +1,11 @@
 package csObject 
 type csPostfix struct{
-  Status	  string `json:"postfix_status"`,
-  Size	    string `json:"postfix_size"`,
-  Position	string `json:"postfix_position"`,
-  Updated	  string `json:"postfix_updated"`,
-  Signature	string `json:"postfix_signature"`,
-  Server_id	string `json:"server_id"`,
-  Id	      string `json:"postfix_id"`,
-  Path	    string `json:"postfix_path"`
+	Status		string 	`json:"postfix_status"`,
+  	Size		string 	`json:"postfix_size"`,
+  	Position	string 	`json:"postfix_position"`,
+  	Updated	  	string 	`json:"postfix_updated"`,
+  	Signature	string 	`json:"postfix_signature"`,
+  	Server_id	string 	`json:"server_id"`,
+  	Id	      	string 	`json:"postfix_id"`,
+  	Path	    string 	`json:"postfix_path"`
 }
